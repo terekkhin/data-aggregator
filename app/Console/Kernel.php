2a94bb2d05ca199f7e3f7b36d8ec36a207c329ef
terekkhin <k1r1llter2004@gmail.com>
2024-04-08 07:33:19 +0300

    add migration, fix laravel, scheduler setup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Controllers\VkDataAggController;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +16,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function (){
+            VkDataAggController::get_vk_data();
+        })->everyMinute();
     }
 
     /**
